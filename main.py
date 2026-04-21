692a9dcc679587119e95e66258d08784dbdcfe8f
M0N1SHGUPTA <2006monishkumar@gmail.com>
2026-04-22 01:47:37 +0530

    Created Static and basic layout of the site.

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name = "static")
+
 templates = Jinja2Templates(directory="templates")
 
-posts : list[dict] = [{
-    "name": "King Monish",
-    "work" : "Rule the World."
-},
-{
-    "name": "Inventor Monish",
-    "work": "Invent New Tech"
+posts: list[dict] = [
+    {
+        "id": 1,
+        "author": "Corey Schafer",
+        "title": "FastAPI is Awesome",
+        "content": "This framework is really easy to use and super fast.",
+        "date_posted": "April 20, 2025",
+    },
+    {
+        "id": 2,
+        "author": "Jane Doe",
+        "title": "Python is Great for Web Development",
+        "content": "Python is a great language for web development, and FastAPI makes it even better.",
+        "date_posted": "April 21, 2025",
+    },
+]
 
-}]
 
 
-@app.get("/", include_in_schema=False)
-@app.get('/posts', include_in_schema=False)
+@app.get('/posts', include_in_schema=False, name = "posts")
+@app.get("/", include_in_schema=False, name = "home")
 def home(requests : Request):
-    return templates.TemplateResponse(requests, 'home.html')
+    return templates.TemplateResponse(requests, 'home.html', {"posts": posts, "title": "Cloud_Blog"})
 
 @app.get("/kings")
 def kings(requests : Request):
-    return templates.TemplateResponse(requests, "home.html", {"posts": posts, "title" : "kings"})
+    return templates.TemplateResponse(requests, "home.html", {"posts": posts, "title" : "Cloud_Blog"})
 
 
 @app.get('/api/kings')
